bf2cad792aea3de0503ae0dc46b073c0683b97f5
Christian Cuadero <christiancuadero@gmail.com>
2025-12-29 11:56:45 +0800

    Update Error.py
    Added another sample function with try-except block

--- Day7/Error.py
+++ Day7/Error.py
@@ -8,4 +8,17 @@ def clean_text(text):
     # Return a TypeError error if the wrong data type was used
     raise TypeError("The clean_text() function expects a string as an argument, please check the data type provided!")
     
-clean_text("User Name 187")
+print(clean_text("User Name 187"))
+
+
+def area(length, width):
+  try:
+      if length == int and width == int:
+        result = length * width
+        return result
+      else:
+        raise ValueError("Invalid Input!")
+  except Exception as e:
+    print(f"An unexpected error occured: {e}")
+
+print(area(3, "f"))
